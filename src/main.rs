a8b6a77339082349fcc700a221489ce1c4be004a
Christof Laenzlinger <laenzlinger@github.com>
2022-12-10 14:09:52 +0100

    Some first idea how to map

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_midi::{MidiIn, MidiMessage, MidiOut};
+use embedded_midi::{Channel, Control, MidiIn, MidiMessage, MidiOut};
 use fugit::HertzU32;
 use nb::block;
 use panic_probe as _;
@@ -73,13 +73,59 @@ fn main() -> ! {
 
     loop {
         if let Ok(event) = block!(midi_in.read()) {
-            midi_out.write(resolve(&event)).ok();
+            info!("received {}", event);
+            match resolve(event) {
+                Some(out) => {
+                    info!("send {}", out);
+                    midi_out.write(&out).ok()
+                }
+                None => None,
+            };
         }
     }
 }
 
-fn resolve(m: &MidiMessage) -> &MidiMessage {
-    return m;
+const PLETHORA_CHANNEL: Channel = Channel::new(1);
+const XTONE_CHANNEL: Channel = Channel::new(0);
+
+const HIGH_VALUE: midi_types::Value7 = midi_types::Value7::new(127);
+
+const BOARD_UP: MidiMessage =
+    MidiMessage::ControlChange(PLETHORA_CHANNEL, Control::new(95), HIGH_VALUE);
+const BOARD_DOWN: MidiMessage =
+    MidiMessage::ControlChange(PLETHORA_CHANNEL, Control::new(94), HIGH_VALUE);
+
+fn resolve(event: MidiMessage) -> Option<MidiMessage> {
+    match event {
+        embedded_midi::MidiMessage::ControlChange(channel, control, _value) => {
+            if XTONE_CHANNEL == channel {
+                return resolve_xtone(control);
+            } else {
+                return None;
+            }
+        }
+        _ => {
+            return None;
+        }
+    }
 }
 
-// End of file
+const XTONE_GREEN: [Control; 6] = [
+    Control::new(10),
+    Control::new(22),
+    Control::new(11),
+    Control::new(24),
+    Control::new(25),
+    Control::new(26),
+];
+
+const XTONE_GREEN_A: Control = XTONE_GREEN[0];
+const XTONE_GREEN_C: Control = XTONE_GREEN[3];
+
+fn resolve_xtone(control: Control) -> Option<MidiMessage> {
+    match control {
+        XTONE_GREEN_A => Some(BOARD_DOWN),
+        XTONE_GREEN_C => Some(BOARD_UP),
+        _ => None,
+    }
+}
