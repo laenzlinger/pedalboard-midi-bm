00b01ede2de56a0bb8ac92069a43eeddae0587d1
Christof Laenzlinger <laenzlinger@github.com>
2022-12-16 11:48:02 +0100

    assign xtone buttons as specified

--- src/pedalboard/xtone.rs
+++ src/pedalboard/xtone.rs
@@ -30,19 +30,19 @@ pub const NONE: Vec<MidiMessage, 8> = Vec::new();
 
 pub fn resolve_xtone(control: Control) -> Vec<MidiMessage, 8> {
     match control {
-        XTONE_GREEN_A => Plethora::BoardDown.midi_messages(),
-        XTONE_GREEN_B => NONE,
-        XTONE_GREEN_C => Plethora::Board(29).midi_messages(),
+        XTONE_GREEN_A => Plethora::Board(29).midi_messages(),
+        XTONE_GREEN_B => Plethora::Board(30).midi_messages(),
+        XTONE_GREEN_C => NONE,
         XTONE_GREEN_D => Plethora::BoardUp.midi_messages(),
-        XTONE_GREEN_E => NONE,
-        XTONE_GREEN_F => Plethora::Board(30).midi_messages(),
+        XTONE_GREEN_E => Plethora::BoardDown.midi_messages(),
+        XTONE_GREEN_F => NONE,
 
-        XTONE_BLUE_A => RC500::MemUp().midi_messages(),
-        XTONE_BLUE_B => RC500::MemDown().midi_messages(),
-        XTONE_BLUE_C => RC500::ClearCurrent().midi_messages(),
-        XTONE_BLUE_D => RC500::ToggleRhythm().midi_messages(),
-        XTONE_BLUE_E => RC500::RhythmVariation().midi_messages(),
-        XTONE_BLUE_F => RC500::LoopEffect().midi_messages(),
+        XTONE_BLUE_A => RC500::ToggleRhythm().midi_messages(),
+        XTONE_BLUE_B => RC500::RhythmVariation().midi_messages(),
+        XTONE_BLUE_C => RC500::LoopEffect().midi_messages(),
+        XTONE_BLUE_D => RC500::MemUp().midi_messages(),
+        XTONE_BLUE_E => RC500::MemDown().midi_messages(),
+        XTONE_BLUE_F => RC500::ClearCurrent().midi_messages(),
 
         XTONE_RED_A => NONE,
         XTONE_RED_B => NONE,
